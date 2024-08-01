7a318831033b996401cf6cefd5ad13ff959eb265
alanb4rt <al4n.b4rt@gmail.com>
2024-08-01 15:29:14 +0200

    style html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import EmptyResult from "./components/EmptyResult";
 import Result from "./components/Result";
+import IconCalculator from "./assets/images/icon-calculator.svg";
 
 export default function App() {
   const [showResult, setShowResult] = useState(false);
@@ -12,52 +13,77 @@ export default function App() {
   return (
     <>
       <main className="container mx-auto">
-        <div className="grid grid-cols-2">
-          <div>
-            <form onSubmit={handleSubmit}>
-              <h1 className="text-2xl font-bold text-mySlate-900">
-                Mortgage Calculator
-              </h1>
-              <button type="reset">Clear All</button>
-              <div>
-                <label htmlFor="amount">Mortgage Amount</label>
-                <input type="number" name="amount" id="amount" />
-              </div>
-              <div>
-                <label htmlFor="term">Mortgage Term</label>
-                <input type="number" name="term" id="term" />
-              </div>
-              <div>
-                <label htmlFor="rate">Interest Rate</label>
-                <input type="number" name="rate" id="rate" />
-              </div>
-              <div>
-                <label>Mortgage Type</label>
-                <div>
-                  <input
-                    type="radio"
-                    id="repayment"
-                    name="type"
-                    value="repayment"
-                  />
-                  <label htmlFor="repayment">Repayment</label>
+        <div className="grid place-content-center h-screen">
+          <div className="grid grid-cols-2 bg-white rounded-2xl overflow-hidden">
+            <div className="p-8">
+              <form onSubmit={handleSubmit}>
+                <div className="flex justify-between mb-4">
+                  <h1 className="text-2xl font-bold text-mySlate-900">
+                    Mortgage Calculator
+                  </h1>
+                  <button className="btn-reset underline" type="reset">
+                    Clear All
+                  </button>
                 </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="interest"
-                    name="type"
-                    value="interest"
-                  />
-                  <label htmlFor="interest">Interest Only</label>
+                <div className="ctn-input">
+                  <label htmlFor="amount">Mortgage Amount</label>
+                  <div className="ctn-input-content">
+                    <div className="input-content-info">£</div>
+                    <input type="number" name="amount" id="amount" />
+                  </div>
                 </div>
-              </div>
-              <button onClick={() => setShowResult(!showResult)}>
-                Calculate Repayments
-              </button>
-            </form>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="ctn-input">
+                    <label htmlFor="term">Mortgage Term</label>
+                    <div className="ctn-input-content">
+                      <input type="number" name="term" id="term" />
+                      <div className="input-content-info">years</div>
+                    </div>
+                  </div>
+                  <div className="ctn-input">
+                    <label htmlFor="rate">Interest Rate</label>
+                    <div className="ctn-input-content">
+                      <input type="number" name="rate" id="rate" />
+                      <div className="input-content-info">%</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="ctn-input">
+                  <fieldset>
+                    <legend className="mb-2">Mortgage Type</legend>
+                    <label className="ctn-input-radio" htmlFor="repayment">
+                      <input
+                        type="radio"
+                        id="repayment"
+                        name="type"
+                        value="repayment"
+                      />
+                      Repayment
+                    </label>
+                    <label className="ctn-input-radio" htmlFor="interest">
+                      <input
+                        type="radio"
+                        id="interest"
+                        name="type"
+                        value="interest"
+                      />
+                      Interest Only
+                    </label>
+                  </fieldset>
+                </div>
+                <button
+                  className="btn-submit"
+                  onClick={() => setShowResult(!showResult)}
+                >
+                  <img src={IconCalculator} alt="" />
+                  Calculate Repayments
+                </button>
+              </form>
+            </div>
+            <div className="bg-mySlate-900 text-white p-8 rounded-bl-[4rem]">
+              {!showResult ? <EmptyResult /> : <Result />}
+            </div>
           </div>
-          <div>{!showResult ? <EmptyResult /> : <Result />}</div>
         </div>
       </main>
     </>
--- src/components/EmptyResult.jsx
+++ src/components/EmptyResult.jsx
@@ -3,10 +3,10 @@ import IconEmpty from "../assets/images/illustration-empty.svg";
 export default function EmptyResult() {
   return (
     <>
-      <div className="text-center">
+      <div className="flex flex-col justify-center gap-2 h-full text-center">
         <img className="mx-auto" src={IconEmpty} alt="Illustration" />
         <h2 className="text-2xl font-bold">Results shown here</h2>
-        <p>
+        <p className="text-mySlate-500">
           Complete the form and click “calculate repayments” to see what your
           monthly repayments would be.
         </p>
